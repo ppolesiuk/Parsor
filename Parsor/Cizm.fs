822697fb1c942f40b84c9ac4969ed42eb3bb4309
poles-p <poles_p@wp.pl>
2010-09-23 13:36:55 +0200

    Start with C-izm

---
+++ Parsor/Cizm.fs
@@ -0,0 +1,24 @@
+﻿module Parsor.Cizm
+
+open Parsor.Core
+open Parsor.Combinators
+open Parsor.Primitives
+
+let cComment : Parsor<char, unit> =
+    skipChar '/' .>>
+    (
+        (skipChar '/' <!> fun() -> skipMany (notFollowedBy(skipChar '\n') "new line" .>> getToken) >>. skipChar '\n') ^|
+        (skipChar '*' <!> fun() -> skipMany (notFollowedBy(skipString "*/") "close comment sequence" .>> getToken) >>. skipString "*/") ^|
+        fatalError "Invalid comment startup sequence."
+    )
+
+let cWhiteSpace : Parsor<char, unit> =
+    let rec wsParse arg =
+        (
+        whiteSpace .>>
+        (
+            (cComment <!> fun() -> wsParse) ^|
+            parsor.Return ()
+        )) arg
+    in
+        wsParse
